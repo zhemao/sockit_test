9cb9f651bb244b3bc856b22ef1fd724e96d0237c
Howard Mao <zhehao.mao@gmail.com>
2013-12-22 21:23:05 -0500

    make sure oneshot detects correct edge

--- rtl/oneshot.v
+++ rtl/oneshot.v
@@ -11,7 +11,7 @@ output level_sig;
 reg cur_value;
 reg last_value;
 
-assign level_sig = cur_value && !last_value;
+assign level_sig = !cur_value && last_value;
 
 always @(posedge clk) begin
     cur_value <= edge_sig;
