b149ad5ec798ca60a53e73baf002219a40147a0f
Howard Mao <zhehao.mao@gmail.com>
2013-12-23 16:03:36 -0500

    simplify blinker logic

--- rtl/blinker.v
+++ rtl/blinker.v
@@ -14,41 +14,36 @@ output reg [3:0] led;
 input reset;
 input pause;
 
-reg [23:0] count;
-reg [1:0] pos;
+reg [23:0] count = 24'b0;
+reg [2:0] pos = 3'b000;
 reg running = 1'b1;
-reg up;
 
 always @(pos) begin
     case (pos)
-        2'b00: led <= 4'b0001;
-        2'b01: led <= 4'b0010;
-        2'b10: led <= 4'b0100;
-        2'b11: led <= 4'b1000;
+        3'b000: led <= 4'b0001;
+        3'b001: led <= 4'b0010;
+        3'b010: led <= 4'b0100;
+        3'b011: led <= 4'b1000;
+        3'b100: led <= 4'b0100;
+        3'b101: led <= 4'b0010;
+        default: led <= 4'b0000;
     endcase
 end
 
 always @(posedge clk) begin
     if (reset) begin
         count <= 24'b0;
-        pos <= 2'b00;
-        up <= 1'b0;
+        pos <= 3'b000;
+        running <= 1'b1;
     end else if (pause) begin
         running <= !running;
     end else if (running) begin
         if (count == 24'b0) begin
             count <= {delay, 20'b0};
-            if (up) begin
-                if (pos == 2'b11)
-                    up <= 1'b0;
-                else
-                    pos <= pos + 1'b1;
-            end else begin
-                if (pos == 2'b00)
-                    up <= 1'b1;
-                else
-                    pos <= pos - 1'b1;
-            end
+            if (pos == 3'b101)
+                pos <= 3'b000;
+            else
+                pos <= pos + 1'b1;
         end else begin
             count <= count - 1'b1;
         end
