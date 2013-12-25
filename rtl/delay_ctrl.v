64ec3294517a29293a47488d0d1b098a345e3d5c
Howard Mao <zhehao.mao@gmail.com>
2013-12-25 15:34:13 -0500

    set up soc system for controlling LEDs from Linux

--- rtl/delay_ctrl.v
+++ rtl/delay_ctrl.v
@@ -1,9 +1,16 @@
 module delay_ctrl (
     clk,
+    reset,
+
     faster,
     slower,
     delay,
-    reset
+
+    read,
+    write,
+    chipselect,
+    writedata,
+    readdata
 );
 
 input clk;
@@ -12,13 +19,22 @@ input slower;
 output [3:0] delay;
 input reset;
 
+input read;
+input write;
+input chipselect;
+input [7:0] writedata;
+output [7:0] readdata;
+
 reg [3:0] delay_intern = 4'b1000;
 
 assign delay = delay_intern;
+assign readdata = {4'b0000, delay_intern};
 
 always @(posedge clk) begin
     if (reset)
         delay_intern <= 4'b1000;
+    else if (chipselect && write)
+        delay_intern <= writedata[3:0];
     else if (faster && delay_intern != 4'b0001)
         delay_intern <= delay_intern - 1'b1;
     else if (slower && delay_intern != 4'b1111)
