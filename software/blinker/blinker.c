8fe6d5482833e27faa2506241a00a27965ab0d6f
Howard Mao <zhehao.mao@gmail.com>
2013-12-28 23:40:59 -0500

    trying to implement sysfs filesystem for blinker module

--- software/blinker/blinker.c
+++ software/blinker/blinker.c
@@ -1,30 +1,84 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/device.h>
+#include <linux/platform_device.h>
+#include <linux/uaccess.h>
+#include <linux/ioport.h>
+#include <linux/io.h>
 
-volatile char *blinker_mem;
+#define LWHPS2FPGA_BASE 0xff200000
+#define BLINKER_OFFSET 0x0
+#define BLINKER_BASE (LWHPS2FPGA_BASE + BLINKER_OFFSET)
+
+static struct device_driver blinker_driver = {
+	.name = "blinker",
+	.bus = &platform_bus_type,
+};
+
+ssize_t blinker_show(struct device_driver *drv, char *buf)
+{
+	return 0;
+}
+
+ssize_t blinker_store(struct device_driver *drv, const char *buf, size_t count)
+{
+	u8 delay;
+	int ret;
+
+	if (buf == NULL) {
+		pr_err("Error, string must not be NULL\n");
+		return -EINVAL;
+	}
+
+	if ((ret = kstrtou8(buf, 10, &delay)) < 0) {
+		pr_err("Could not convert string to integer\n");
+		return ret;
+	}
+
+	if (delay < 1 || delay > 15) {
+		pr_err("Invalid delay %d\n", delay);
+		return -EINVAL;
+	}
+
+	outb(delay, BLINKER_BASE);
+
+	return count;
+}
+
+static DRIVER_ATTR(blinker, S_IWUSR, blinker_show, blinker_store);
 
 MODULE_LICENSE("Dual BSD/GPL");
 
-static int blinker_init(void)
+static int __init blinker_init(void)
 {
-	unsigned long blink_base = (1 << 32);
+	int ret;
+	struct resource *res;
 
-	pr_info("Loaded blinker module.\n");
+	if ((ret = driver_register(&blinker_driver)) < 0)
+		return ret;
 
-	blinker_mem = phys_to_virt(blink_base);
+	ret = driver_create_file(&blinker_driver, &driver_attr_blinker);
+	if (ret < 0) {
+		driver_unregister(&blinker_driver);
+		return ret;
+	}
 
-	pr_info("Current blinker delay is %d\n", *blinker_mem);
-
-	*blinker_mem = 0x2;
+	res = request_region(BLINKER_BASE, 1, "blinker");
+	if (res == NULL) {
+		driver_remove_file(&blinker_driver, &driver_attr_blinker);
+		driver_unregister(&blinker_driver);
+		return -EBUSY;
+	}
 
 	return 0;
 }
 
-static void blinker_exit(void)
+static void __exit blinker_exit(void)
 {
-	*blinker_mem = 0x4;
-	pr_info("Unloaded blinker module.\n");
+	driver_remove_file(&blinker_driver, &driver_attr_blinker);
+	driver_unregister(&blinker_driver);
+	release_region(BLINKER_BASE, 1);
 }
 
 module_init(blinker_init);
